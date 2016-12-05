fb1a832dc73886c67739d072cdbf1f173fe66b7f
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-12-05 16:44:07 +0100

    Simplification, ça marche ''presque''

--- graphs.py
+++ graphs.py
@@ -36,8 +36,7 @@ for k in range(0,len(adjcoeff)):
         adjcoeff[k] += (0,)
 print ('There is %i' % len(adjcoeff) + ' partitions')
 print adjcoeff
-numdiag = len(adjcoeff)
-print numdiag
+numpart = len(adjcoeff)
 
 def swap(xs, a, b):
     xs[a], xs[b] = xs[b], xs[a]
@@ -52,22 +51,21 @@ def derange(xs):
 norder = int(raw_input('Order of the diagrams ?\n'))
 adjmtrx = []
 ## Initialisation
-print 'numdiag'
-print numdiag
-for i in range (0,numdiag):
-    adjmtrx.append(np.zeros((norder, norder)))
+print 'numpart'
+print numpart
+
+
+for partition in adjcoeff:
+    adjmtrx.append((partition*norder+tuple([0]*(norder**2-len(partition*norder)))))
 
 
 icount = 0
-for element in adjcoeff:
-    #element = list(element)
-    element = deque(element)
-    element.extendleft([0] *(norder - len(element)))
-
-
-
-
-
+#for element in adjcoeff:
+#    #element = list(element)
+#    element = deque(element)
+#    element.extendleft([0] *(norder - len(element)))
+#    print element
+#
 
 
 class unique_element:
@@ -94,11 +92,8 @@ def perm_unique_helper(listunique,result_list,d):
                 i.occurrences+=1
 
 
-poss_lines = [[] for _ in range(norder)]
-poss_lines.remove(0)
-
-
-
+#poss_lines = [[] for _ in range(norder)]
+#poss_lines.remove(0)
 
 
 
@@ -109,69 +104,71 @@ poss_lines.remove(0)
 #            adjmtrx[icount][line] = element 
 #    icount += 1
 
-print adjcoeff
 #print adjmtrx
+print "Size,",len(adjmtrx[0])
+print adjmtrx[0]
+diagrams = []
 print 'Tri en cours'
 for diagram in adjmtrx:
-    while True:
+    permuts = perm_unique(diagram)
+    for permutation in permuts:
+        matpermut = np.array(permutation).reshape(norder,norder)
         sumcol = []
+        sumrow = []
         for col in range(0,norder):
-            sumcol.append(diagram[:,col].sum())
+            sumcol.append(matpermut[:,col].sum())
+            sumrow.append(matpermut[col,:].sum())
         t1 = all(va == nbody for va in sumcol)
-        t2 = (diagram.trace() == 0)
-        t3 = (diagram[line].sum() == nbody)
-        #print "Somme col",t1,"Trace nulle ?",t2,"Somme sur ligne ?",t3
-        if (not t1 or not t2 or not t3):
-            sumcol = []
-            for line in range(0,norder):
-                random.shuffle(diagram[line])
-            continue
-        else:
+        t2 = (matpermut.trace() == 0)
+        t3 = all(va == nbody for va in sumrow)
+        if (t1 and t2 and t3):
             print "Done !"
-            break
+            print matpermut
+            diagrams.append(matpermut)
 print 'original'
 print adjmtrx[0]
+print "Number of diagrams, ",len(diagrams)
 diaginit = copy.deepcopy(adjmtrx[0])
 ###Column permutations
-while True:
-    #diagpermut = copy.deepcopy(adjmtrx[1])
-    diagpermut = copy.deepcopy(adjmtrx[0])
-    print diagpermut
-    sumcol2 =[]
-    t1 = all(va == nbody for va in sumcol2)
-    t2 = (diagpermut.trace() == 0)
-    t3 = (diagpermut[line].sum() == nbody)
-    eq1 = np.array_equal(diagpermut,diaginit)
-    print eq1
-    #if (not t1 or not t2 or not t3 or eq1):
-    if (not eq1):
-        print 'plop'
-        print diagpermut
-        for col in range(0,norder):
-            random.shuffle(diagpermut[:,col])
-        print 'apres'
-        print diagpermut
-        continue
-    else:
-        break
-
+#while True:
+#    #diagpermut = copy.deepcopy(adjmtrx[1])
+#    diagpermut = copy.deepcopy(adjmtrx[0])
+#    print diagpermut
+#    sumcol2 =[]
+#    t1 = all(va == nbody for va in sumcol2)
+#    t2 = (diagpermut.trace() == 0)
+#    t3 = (diagpermut[line].sum() == nbody)
+#    eq1 = np.array_equal(diagpermut,diaginit)
+#    print eq1
+#    #if (not t1 or not t2 or not t3 or eq1):
+#    if (not eq1):
+#        print 'plop'
+#        print diagpermut
+#        for col in range(0,norder):
+#            random.shuffle(diagpermut[:,col])
+#        print 'apres'
+#        print diagpermut
+#        continue
+#    else:
+#        break
+#
 
 ### Graph part (computing, writing, drawing)        
 G=[]
-for diagram in adjmtrx:
+for diagram in diagrams:
     G.append(nx.from_numpy_matrix(diagram,create_using=nx.MultiDiGraph()))
 
-for i in range(0,numdiag):
+for i in range(0,numpart):
     write_dot(G[i],'diag_%i.dot' % i)
 
-for i in range(0,numdiag):
+for i in range(0,numpart):
     for line in fileinput.FileInput('diag_%i.dot' % i,inplace=1):
         if "digraph" in line:
             line=line.replace(line,line+'splines=true \n sep = 1\n overlap = false;ratio="fill";margin=0;\n' + 
             ' node[label =""]\n 0 [pos = "0,0",shape=circle,fixedsize=true,width=1.2] \n 1 [pos = "0,150",shape=circle,fixedsize=true,width=1]\n 2 [pos = "0,300",shape=circle,fixedsize=true,width=1.2] \n')
         print line,
 
-for i in range(0,numdiag):
+for i in range(0,numpart):
     os.system("neato diag_%i.dot" % i + " -n -Tpng -o diag_%i.png" %i)
 
 
