adgproject/adg

commit 17f3abd2d1175dbb9631cf1f915660453625e0a9

Changes to output and clearer functions
Broke the link between BMBPT and their TSD though...

adg.py
@@ -160,11 +160,18 @@ if theory == "BMBPT":
 numdiag = len(diagrams)
 
 # Treatment of the algebraic expressions
-nb_time_diags = 0
 if theory == "BMBPT" and not norm:
+
+    # Production of the time-structure diagrams
+
     diagrams_time = [tst.TimeStructureDiagram(diagram, diagram.tags[0])
                      for diagram in diagrams]
-    mth.topologically_distinct_diagrams(diagrams_time)
+    tree_TSDs = [tsd for tsd in diagrams_time if tsd.is_tree]
+    non_tree_TSDs = [tsd for tsd in diagrams_time if not tsd.is_tree]
+    mth.topologically_distinct_diagrams(tree_TSDs)
+    nb_tree_TSDs = len(tree_TSDs)
+    mth.topologically_distinct_diagrams(non_tree_TSDs)
+    diagrams_time = tree_TSDs + non_tree_TSDs
     for index, t_diag in enumerate(diagrams_time):
         t_diag.tags.insert(0, index)
         if not t_diag.is_tree:
@@ -173,6 +180,9 @@ if theory == "BMBPT" and not norm:
                                      % tst.tree_time_structure_den(graph)
                                      for graph
                                      in t_diag.equivalent_trees)
+
+    # Production of the BMBPT expressions
+
     for diag in diagrams:
         bmbpt.attribute_qp_labels(diag.graph)
         for t_diag in diagrams_time:
@@ -181,7 +191,6 @@ if theory == "BMBPT" and not norm:
                 diag.tst_is_tree = t_diag.is_tree
                 break
         diag.attribute_expressions(diagrams_time)
-    nb_time_diags = len(diagrams_time)
 
 print "Time ellapsed: ", datetime.now() - start_time
 print "Number of connected diagrams, ", numdiag
@@ -231,7 +240,7 @@ if theory == "BMBPT":
                              nb_2_EHF, nb_2_noHF, nb_3_HF, nb_3_EHF, nb_3_noHF)
     if write_time:
         tst.write_time_diagrams_section(latex_file, directory, pdiag, pdraw,
-                                        diagrams_time)
+                                        diagrams_time, nb_tree_TSDs)
 for diag in diagrams:
     if theory == "BMBPT":
         bmbpt.write_BMBPT_section(latex_file, diag.tags[0], three_N, norm,

bmbpt.py
@@ -240,10 +240,10 @@ def vertex_exchange_sym_factor(diag):
 def write_BMBPT_header(tex_file, numdiag, three_N, norm, nb_2_HF,
                        nb_2_EHF, nb_2_noHF, nb_3_HF, nb_3_EHF, nb_3_noHF):
     """Write overall header for BMBPT result file."""
-    tex_file.write("Valid diagrams: %i\n\n" % numdiag)
-    tex_file.write("2N valid diagrams: %i\n\n" %
-                   (nb_2_HF + nb_2_EHF + nb_2_noHF))
-    tex_file.write("2N canonical diagrams for the energy: %i\n\n" % nb_2_HF)
+    tex_file.write("Valid diagrams: %i\n\n" % numdiag
+                   + "2N valid diagrams: %i\n\n" % (nb_2_HF
+                                                    + nb_2_EHF + nb_2_noHF)
+                   + "2N canonical diagrams for the energy: %i\n\n" % nb_2_HF)
     if not norm:
         tex_file.write(
             "2N canonical diagrams for a generic operator only: %i\n\n"
@@ -265,17 +265,16 @@ def write_BMBPT_section(result, diag_index, three_N, norm,
                         nb_2, nb_2_HF, nb_2_EHF, nb_3_HF, nb_3_EHF):
     """Write section and subsections for BMBPT result file."""
     if diag_index == 0:
-        result.write("\\section{Two-body diagrams}\n\n")
-        result.write("\\subsection{Two-body energy canonical diagrams}\n\n")
+        result.write("\\section{Two-body diagrams}\n\n"
+                     + "\\subsection{Two-body energy canonical diagrams}\n\n")
     elif (diag_index == nb_2_HF) and (not norm):
         result.write("\\subsection{Two-body canonical diagrams for a generic operator only}\n\n")
     elif diag_index == nb_2_HF + nb_2_EHF:
         result.write("\\subsection{Two-body non-canonical diagrams}\n\n")
     if three_N:
         if diag_index == nb_2:
-            result.write("\\section{Three-body diagrams}\n\n")
-            result.write(
-                "\\subsection{Three-body energy canonical diagrams}\n\n")
+            result.write("\\section{Three-body diagrams}\n\n"
+                         + "\\subsection{Three-body energy canonical diagrams}\n\n")
         elif (diag_index == nb_2 + nb_3_HF) and (not norm):
             result.write("\\subsection{Three-body canonical diagrams for a generic operator only}\n\n")
         elif diag_index == nb_2 + nb_3_HF + nb_3_EHF:
@@ -286,10 +285,10 @@ def write_diag_exps(latex_file, bmbpt_diag, norder):
     """Write the expressions associated to a diagram in the LaTeX file."""
     latex_file.write("\\begin{align}\n\\text{PO}%i.%i\n" % (norder,
                                                             (bmbpt_diag.tags[0]
-                                                             + 1)))
-    latex_file.write("&= %s" % bmbpt_diag.feynman_exp
-                     + r" \nonumber \\" + "\n")
-    latex_file.write("&= %s\\end{align}\n" % bmbpt_diag.diag_exp)
+                                                             + 1))
+                     + "&= %s" % bmbpt_diag.feynman_exp
+                     + r" \nonumber \\" + "\n"
+                     + "&= %s\\end{align}\n" % bmbpt_diag.diag_exp)
 
 
 def write_vertices_values(latex_file, diag):
@@ -310,6 +309,7 @@ class BmbptFeynmanDiagram(mth.Diagram):
     def __init__(self, nx_graph, use_norm, tag_num):
         """Generate a BMBPT diagrams using a NetworkX graph."""
         mth.Diagram.__init__(self, nx_graph)
+        self.type = 'BMBPT'
         self.two_or_three_body = 3 if self.max_degree == 6 else 2
         self.tags = [tag_num]
         self.time_tag = -1
@@ -331,7 +331,8 @@ class BmbptFeynmanDiagram(mth.Diagram):
 
     def attribute_expressions(self, time_diags):
         """Attribute the correct Feynman and Goldstone expressions."""
-        self.attribute_vertices_expressions()
+        self.vert_exp = [self.vertex_expression(vertex)
+                         for vertex in self.graph]
         norder = len(self.graph)
         numerator = extract_numerator(self.graph)
         denominator = ""
@@ -370,18 +371,15 @@ class BmbptFeynmanDiagram(mth.Diagram):
             if denominator != "" \
             else "%s%s%s\n" % (prefactor, numerator, extra_factor)
 
-    def attribute_vertices_expressions(self):
-        """Attribute the appropriate expression to each vertex."""
-        vertices_expressions = []
-        for vertex in self.graph:
-            v_exp = r"\epsilon^{" \
-                + "".join("%s"
-                          % self.graph.adj[prop[0]][prop[1]][prop[2]]['qp_state']
-                          for prop in self.graph.in_edges(vertex, keys=True)) \
-                + "}_{" \
-                + "".join("%s"
-                          % self.graph.adj[prop[0]][prop[1]][prop[2]]['qp_state']
-                          for prop in self.graph.out_edges(vertex, keys=True)) \
-                + "}"
-            vertices_expressions.append(v_exp)
-        self.vert_exp = vertices_expressions
+    def vertex_expression(self, vertex):
+        """Return the expression associated to a given vertex."""
+        expression = r"\epsilon^{" \
+            + "".join("%s"
+                      % self.graph.adj[prop[0]][prop[1]][prop[2]]['qp_state']
+                      for prop in self.graph.in_edges(vertex, keys=True)) \
+            + "}_{" \
+            + "".join("%s"
+                      % self.graph.adj[prop[0]][prop[1]][prop[2]]['qp_state']
+                      for prop in self.graph.out_edges(vertex, keys=True)) \
+            + "}"
+        return expression

mbpt.py
@@ -65,6 +65,7 @@ class MbptDiagram(mth.Diagram):
     def __init__(self, mbpt_graph, tag_num):
         """Generate a MBPT diagram using the appropriate NetworkX graph."""
         mth.Diagram.__init__(self, mbpt_graph)
+        self.type = 'MBPT'
         self.tags = [tag_num]
         self.attribute_expression()
 

methods.py
@@ -246,6 +246,7 @@ class Diagram(object):
     """Describes a diagram with its related properties."""
 
     def __init__(self, nx_graph):
+        self.type = 'Diagram'
         self.graph = nx_graph
         self.degrees = sorted([nx_graph.degree(node) for node in nx_graph])
         self.unsort_io_degrees = tuple((nx_graph.in_degree(node),

time_structure.py
@@ -25,9 +25,7 @@ def has_tree_time_structure(graph):
     for vertex_i in diag_copy:
         for vertex_j in diag_copy:
             lgst_path = []
-            for path in nx.all_simple_paths(diag_copy,
-                                            vertex_i,
-                                            vertex_j):
+            for path in nx.all_simple_paths(diag_copy, vertex_i, vertex_j):
                 if len(path) > len(lgst_path):
                     lgst_path = path
             time_diag.add_path(lgst_path)
@@ -50,8 +48,8 @@ def tree_time_structure_den(time_graph):
             else:
                 denominator += "(%s" % time_graph.node[vertex]['label'] \
                     + "".join("+ %s" % time_graph.node[descendant]['label']
-                              for descendant in nx.descendants(time_graph,
-                                                               vertex)) + ")"
+                              for descendant
+                              in nx.descendants(time_graph, vertex)) + ")"
     return denominator
 
 
@@ -72,11 +70,26 @@ def equivalent_labelled_TSDs(equivalent_trees, labelled_TSDs):
     return "".join("%s." % eq_labelled_TSDs.strip(','))
 
 
+def draw_equivalent_tree_TSDs(time_diagram, latex_file):
+    """Draw the equivalent tree TSDs for a given non-tree TSD."""
+    for index, graph in enumerate(time_diagram.equivalent_trees):
+        mth.feynmf_generator(graph,
+                             'MBPT',
+                             'equivalent%i_%i' % (time_diagram.tags[0], index))
+        diag_file = open("equivalent%i_%i.tex" % (time_diagram.tags[0], index))
+        latex_file.write(diag_file.read())
+        diag_file.close()
+        os.unlink("./equivalent%i_%i.tex" % (time_diagram.tags[0], index))
+
+
 def write_time_diagrams_section(latex_file, directory, pdiag, pdraw,
-                                time_diagrams):
+                                time_diagrams, nb_tree_TSDs):
     """Write the appropriate section for tst diagrams in the LaTeX file."""
-    latex_file.write("\\section{Associated time-structure diagrams}\n\n")
+    latex_file.write("\\section{Time-structure diagrams}\n\n")
+    latex_file.write("\\subsection{Tree diagrams}\n\n")
     for tdiag in time_diagrams:
+        if tdiag.tags[0] == nb_tree_TSDs:
+            latex_file.write("\\subsection{Non-tree diagrams}\n\n")
         latex_file.write("\\paragraph{Time-structure diagram T%i:}\n"
                          % (tdiag.tags[0]+1))
         if pdiag and pdraw:
@@ -84,23 +97,14 @@ def write_time_diagrams_section(latex_file, directory, pdiag, pdraw,
                              + "/Diagrams/time_%i.tex" % tdiag.tags[0])
             latex_file.write('\n\\begin{center}\n%s\n\\end{center}\n\n'
                              % time_file.read())
-        latex_file.write("Tree: Yes\n\n" if tdiag.is_tree else "Tree: No\n\n")
-        latex_file.write("\\begin{equation}\n%s\\end{equation}\n" % tdiag.expr)
+        latex_file.write("\\begin{equation}\n\\text{T%i} = %s\\end{equation}\n"
+                         % (tdiag.tags[0]+1, tdiag.expr))
         if not tdiag.is_tree:
             latex_file.write("Equivalent tree diagrams: %s\n\n"
                              % equivalent_labelled_TSDs(tdiag.equivalent_trees,
                                                         time_diagrams))
             latex_file.write('\n\\begin{center}\n')
-            for index, graph in enumerate(tdiag.equivalent_trees):
-                mth.feynmf_generator(graph,
-                                     'MBPT',
-                                     'equivalent%i_%i' % (tdiag.tags[0],
-                                                          index))
-                diag_file = open("equivalent%i_%i.tex" % (tdiag.tags[0],
-                                                          index))
-                latex_file.write(diag_file.read())
-                diag_file.close()
-                os.unlink("./equivalent%i_%i.tex" % (tdiag.tags[0], index))
+            draw_equivalent_tree_TSDs(tdiag, latex_file)
             latex_file.write('\n\\end{center}\n\n')
         feynman_diags = ",".join(" %i" % (tag+1) for tag in tdiag.tags[1:])
         latex_file.write("Related Feynman diagrams:%s.\n\n" % feynman_diags)
@@ -181,6 +185,7 @@ class TimeStructureDiagram(mth.Diagram):
     def __init__(self, bmbpt_diag, tag_num):
         """Generate a TST diagram out of a BMBPT one."""
         mth.Diagram.__init__(self, time_structure_graph(bmbpt_diag.graph))
+        self.type = 'TSD'
         self.tags = [tag_num]
         self.equivalent_trees = []
         if nx.is_arborescence(self.graph):

commit 422289a6a8f307bf760695657c9eaa40ef236d52

Fixed a bug introduced in 17f3abd2

adg.py
@@ -186,7 +186,7 @@ if theory == "BMBPT" and not norm:
     for diag in diagrams:
         bmbpt.attribute_qp_labels(diag.graph)
         for t_diag in diagrams_time:
-            if diag.tags[0] in t_diag.tags:
+            if diag.tags[0] in t_diag.tags[1:]:
                 diag.time_tag = t_diag.tags[0]
                 diag.tst_is_tree = t_diag.is_tree
                 break