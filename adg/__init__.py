fc928785fba13b58ec029c900601b537bfce6b8e
Pierre Arthuis <pierre.arthuis@cea.fr>
2018-06-14 18:08:09 +0200

    Prepare version 0.1.0

--- adg/__init__.py
+++ adg/__init__.py
@@ -1,6 +1,6 @@
 """Information on the ADG module."""
 
 __license__ = ''
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = __maintainer__ = 'Pierre Arthuis'
 __email__ = 'pierre.arthuis@pm.me'
