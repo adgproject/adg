db28e19c01f37d96c7d1e808fab2da03bc70cb18
Pierre Arthuis <pierre.arthuis@cea.fr>
2018-06-19 23:46:08 +0200

    Add a test for to_skeleton

--- adg/tests/test_diag.py
+++ adg/tests/test_diag.py
@@ -1,6 +1,7 @@
 """Tests for the adg.diag module."""
 
 import pytest
+import networkx as nx
 import adg.diag
 
 
@@ -29,3 +30,25 @@ def test_propagator_style():
     # Test an unimplemented case
     with pytest.raises(KeyError):
         adg.diag.propagator_style('prop_pp')
+
+
+def test_to_skeleton():
+    """Test the function returning the skeleton of a graph."""
+    # Test for a graph with one redundant link
+    graph = nx.MultiDiGraph()
+    graph.add_edges_from([(0, 1), (1, 2), (0, 2)])
+    assert list(adg.diag.to_skeleton(graph).edges()) == [(0, 1), (1, 2)]
+
+    # Test for a graph without redundant link
+    graph = nx.MultiDiGraph()
+    graph.add_edges_from([(0, 1), (1, 2), (0, 3)])
+    assert list(adg.diag.to_skeleton(graph).edges()) == [(0, 1), (0, 3), (1, 2)]
+
+    # Test for a graph with twice the same edge
+    graph = nx.MultiDiGraph()
+    graph.add_edges_from([(0, 1), (1, 2), (1, 2)])
+    assert list(adg.diag.to_skeleton(graph).edges()) == [(0, 1), (1, 2)]
+
+    # Test for an empty graph
+    graph = nx.MultiDiGraph()
+    assert list(adg.diag.to_skeleton(graph).edges()) == []
