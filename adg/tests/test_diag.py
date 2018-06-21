ad720a537e6c95bccf1ef5665facc58d166d18f7
Pierre Arthuis <pierre.arthuis@cea.fr>
2018-06-21 14:48:44 +0200

    Add a test for feynmf_generator

--- adg/tests/test_diag.py
+++ adg/tests/test_diag.py
@@ -1,8 +1,72 @@
 """Tests for the adg.diag module."""
 
+import os
 import pytest
 import networkx as nx
+import numpy as np
 import adg.diag
+
+
+def test_feynmf_generator():
+    """Test the feynmf_generator routine."""
+    graph = nx.from_numpy_matrix(np.array([[0, 6], [0, 0]]),
+                                 create_using=nx.MultiDiGraph(),
+                                 parallel_edges=True)
+    adg.diag.label_vertices([graph], "BMBPT")
+    adg.diag.feynmf_generator(graph, "BMBPT", "diag0")
+    feynmf_ref = "\\parbox{40pt}{\\begin{fmffile}{diag0}\n" \
+        + "\\begin{fmfgraph*}(40,40)\n" \
+        + "\\fmfcmd{style_def prop_pm expr p =\n" \
+        + "draw_plain p;\n" \
+        + "shrink(.7);\n" \
+        + "\tcfill (marrow (p, .25));\n" \
+        + "\tcfill (marrow (p, .75))\n" \
+        + "endshrink;\n" \
+        + "enddef;}\n" \
+        + "\\fmftop{v1}\\fmfbottom{v0}\n" \
+        + "\\fmf{phantom}{v0,v1}\n" \
+        + "\\fmfv{d.shape=square,d.filled=full,d.size=3thick}{v0}\n" \
+        + "\\fmfv{d.shape=circle,d.filled=full,d.size=3thick}{v1}\n" \
+        + "\\fmffreeze\n" \
+        + "\\fmf{prop_pm,left=0.9}{v0,v1}\n" \
+        + "\\fmf{prop_pm,right=0.9}{v0,v1}\n" \
+        + "\\fmf{prop_pm,left=0.75}{v0,v1}\n" \
+        + "\\fmf{prop_pm,right=0.75}{v0,v1}\n" \
+        + "\\fmf{prop_pm,left=0.5}{v0,v1}\n" \
+        + "\\fmf{prop_pm,right=0.5}{v0,v1}\n" \
+        + "\\end{fmfgraph*}\n" \
+        + "\\end{fmffile}}\n"
+
+    with open("diag0.tex", "r") as result_file:
+        assert result_file.read() == feynmf_ref
+    os.unlink("diag0.tex")
+
+    graph = nx.from_numpy_matrix(np.array([[0, 1], [1, 0]]),
+                                 create_using=nx.MultiDiGraph(),
+                                 parallel_edges=True)
+    adg.diag.label_vertices([graph], "MBPT")
+    adg.diag.feynmf_generator(graph, "MBPT", "diag1")
+    feynmf_ref = "\\parbox{40pt}{\\begin{fmffile}{diag1}\n" \
+        + "\\begin{fmfgraph*}(40,40)\n" \
+        + "\\fmfcmd{style_def half_prop expr p =\n" \
+        + "draw_plain p;\n" \
+        + "shrink(.7);\n" \
+        + "\tcfill (marrow (p, .5))\n" \
+        + "endshrink;\n" \
+        + "enddef;}\n" \
+        + "\\fmftop{v1}\\fmfbottom{v0}\n" \
+        + "\\fmf{phantom}{v0,v1}\n" \
+        + "\\fmfv{d.shape=circle,d.filled=full,d.size=3thick}{v0}\n" \
+        + "\\fmfv{d.shape=circle,d.filled=full,d.size=3thick}{v1}\n" \
+        + "\\fmffreeze\n" \
+        + "\\fmf{half_prop,right=0.5}{v0,v1}\n" \
+        + "\\fmf{half_prop,right=0.5}{v1,v0}\n" \
+        + "\\end{fmfgraph*}\n" \
+        + "\\end{fmffile}}\n"
+
+    with open("diag1.tex", "r") as result_file:
+        assert result_file.read() == feynmf_ref
+        os.unlink("diag1.tex")
 
 
 def test_propagator_style():
