adgproject/adg

commit b3684c5f73d15b81cf0986bfd5ffcb30ab60c01f

Avoid topologically equivalent PBMBPT diagrams from different BMBPT ones

adg/run.py
@@ -256,6 +256,7 @@ def generate_diagrams(commands, id_generator):
             adg.diag.topologically_distinct_diagrams(new_diags)
             del diagrams[idx]
             diagrams += new_diags
+    adg.diag.topologically_distinct_diagrams(diagrams)
     return diagrams
 
 

commit 591edc6428857d322503334dcbc9f175c750a957

Fix a bug introduced in b3684c5f for MBPT diagrams

adg/run.py
@@ -256,7 +256,7 @@ def generate_diagrams(commands, id_generator):
             adg.diag.topologically_distinct_diagrams(new_diags)
             del diagrams[idx]
             diagrams += new_diags
-    adg.diag.topologically_distinct_diagrams(diagrams)
+        adg.diag.topologically_distinct_diagrams(diagrams)
     return diagrams
 
 